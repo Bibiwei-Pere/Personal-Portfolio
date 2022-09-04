489ea8f266fba70c0915ce82df876f2ce89d14bd
Adrenaline Nine <nine.adrenaline@gmail.com>
2022-09-04 21:43:52 +0100

    Update Footer.js

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
-        Akram Sakib
+        Bibiwei Pere
         . All Rights Reserved.
       </span>
     </footer>
