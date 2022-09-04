4a80ea3af8a1960ba8c8259f7a5f97966590375b
Adrenaline9 <nine.adrenaline@gmail.com>
2022-09-04 13:09:49 +0100

    1 file modified

--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -1,48 +1,47 @@
-import React, { useEffect, useState } from "react";
-import Filter from "./Filter/Filter";
-import Project from "./Project/Project";
-import { AnimatePresence, motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React, { useEffect, useState } from 'react';
+import Filter from './Filter/Filter';
+import Project from './Project/Project';
+import { AnimatePresence, motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
   const [filtered, setFiltered] = useState([]);
-  const [active, setActive] = useState("all");
+  const [active, setActive] = useState('all');
 
   useEffect(() => {
     fetchProject();
   }, []);
 
   const fetchProject = async () => {
-    const res = await fetch("./projects.json");
+    const res = await fetch('./projects.json');
     const data = await res.json();
     setProjects(data.result);
     setFiltered(data.result);
   };
 
-     const { ref, inView } = useInView({
-       threshold: 0.2,
-       triggerOnce: true,
-     });
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
 
-     const [viewDiv, setViewDiv] = useState(false);
+  const [viewDiv, setViewDiv] = useState(false);
 
-     const animation = useAnimation();
+  const animation = useAnimation();
 
-     useEffect(() => {
-       if (inView) {
-         setViewDiv(true);
-       }
-       if (!inView) {
-         setViewDiv(false);
-       }
-     }, [inView, animation]);
+  useEffect(() => {
+    if (inView) {
+      setViewDiv(true);
+    }
+    if (!inView) {
+      setViewDiv(false);
+    }
+  }, [inView, animation]);
 
   return (
     <>
       <section className="container px-6 mx-auto py-20" ref={ref}>
-        <h2 className="mb-4 text-4.5xl text-center tracking-tight font-extrabold  text-dark dark:text-white sm:leading-none"
->
+        <h2 className="mb-4 text-4.5xl text-center tracking-tight font-extrabold  text-dark dark:text-white sm:leading-none">
           Something that he has
           <span className="text-indigo-600 dark:text-indigo-500"> build</span>
         </h2>
