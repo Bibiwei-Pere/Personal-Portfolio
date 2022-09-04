afeb76f86c5b3e991b0d697ac04b793debc1f1c2
Adrenaline Nine <nine.adrenaline@gmail.com>
2022-09-04 13:22:32 +0100

    Update Banner.js

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -85,9 +85,9 @@ const Banner = () => {
                 rel="noreferrer"
                 className="dark:text-white dark:bg-slate-700"
                 target="_blank"
-                href="www.bibiweijnr@gmail.com"
+                href="mailto:bibiweijnr@gmail.com"
               >
-                <i className="fa-brands fa-envelope"></i>
+                <i className="fa fa-envelope"></i>
               </a>
             </div>
             <button className="rounded w-24 bg-indigo-700 hover:bg-indigo-800 py-2 px-4 text-white">
