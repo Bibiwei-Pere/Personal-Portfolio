a4fe57dc82014b83103fc566eeeb6c870483bc95
Adrenaline Nine <nine.adrenaline@gmail.com>
2022-09-05 10:26:07 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
         {/* This example requires Tailwind CSS v2.0+ */}
         <div className="relative z-50 bg-white dark:bg-slate-900 transition-all">
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
-            <div className="flex justify-between items-center transition-all dark:border-slate-700 border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-between items-center transition-all dark:border-slate-700 border-b-2 border-gray-100 py-6">
               <motion.div
                 initial="hidden"
                 animate="visible"
@@ -63,7 +63,7 @@ const Navbar = () => {
                   </svg>
                 </button>
               </div>
-              <nav className="hidden md:flex space-x-10 md:justify-center">
+              <nav className="hidden md:flex space-x-10">
                 <div className="relative">
                   {/* Item active: "text-gray-900", Item inactive: "text-gray-500" */}
 
