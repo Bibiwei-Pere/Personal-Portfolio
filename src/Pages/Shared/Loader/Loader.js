684a131c1a8f8badc6c98ceb316c5a1f69b7a71e
Adrenaline Nine <nine.adrenaline@gmail.com>
2022-09-05 10:05:44 +0100

    Update Loader.js

--- src/Pages/Shared/Loader/Loader.js
+++ src/Pages/Shared/Loader/Loader.js
@@ -11,38 +11,26 @@ const Loader = () => {
               <div className="spinner"></div>
 
               <div className="txt-loading">
-                <span data-text-preloader="A" className="letters-loading">
-                  A
-                </span>
-                <span data-text-preloader="R" className="letters-loading">
-                  K
-                </span>
-                <span data-text-preloader="C" className="letters-loading">
-                  R
-                </span>
-                <span data-text-preloader="R" className="letters-loading">
-                  A
-                </span>
-                <span data-text-preloader="E" className="letters-loading">
-                  M
+                <span data-text-preloader="P" className="letters-loading">
+                  B
                 </span>
                 <span data-text-preloader="" className="letters-loading">
-                  {" "}
-                </span>
-                <span data-text-preloader="A" className="letters-loading">
-                  S
-                </span>
-                <span data-text-preloader="T" className="letters-loading">
-                  A
-                </span>
-                <span data-text-preloader="I" className="letters-loading">
-                  K
-                </span>
-                <span data-text-preloader="V" className="letters-loading">
                   I
                 </span>
                 <span data-text-preloader="E" className="letters-loading">
                   B
+                </span>
+                <span data-text-preloader="" className="letters-loading">
+                  I
+                </span>
+                <span data-text-preloader="R" className="letters-loading">
+                  W
+                </span>
+                <span data-text-preloader="" className="letters-loading">
+                  E
+                </span>
+                <span data-text-preloader="E" className="letters-loading">
+                  I
                 </span>
               </div>
             </div>
