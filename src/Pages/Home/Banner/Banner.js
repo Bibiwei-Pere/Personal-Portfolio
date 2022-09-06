8f6fd1c7dbdb7cf74857410bfbc9953bffa4c0e7
Adrenaline Nine <nine.adrenaline@gmail.com>
2022-09-06 09:39:39 +0100

    Update Banner.js

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -21,7 +21,7 @@ const Banner = () => {
   });
 
   return (
-    <section ref={ref} className="container px-6 mx-auto relative pb-8">
+    <section ref={ref} className="container px-6 mx-auto relative pt-20 pb-8">
       <div className="flex flex-col lg:flex-row pt-8 md:pt-16 lg:pt-20 px-6 lg:justify-between">
           <h2 className="mb-2 text-4xl text-left tracking-tight font-extrabold  text-dark dark:text-white sm:leading-none">
             Web Application
