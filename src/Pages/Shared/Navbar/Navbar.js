1e1c4debb8a0b4f896ff1f94daf2898b0d393959
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 23:36:20 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -37,9 +37,9 @@ const Navbar = () => {
                     alt="logo"
                   />
                 </Link>
-              </motion.div>
-              <div className="-mr-2 -my-2 md:hidden flex items-center justify-between">
-<ThemeToggle />
+              </motion.div><ThemeToggle />
+              <div className="-mr-2 -my-2 md:hidden">
+
                 <button
                   type="button"
                   className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
