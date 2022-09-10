eb0795ca19ba260fd4bcf3739a7eab41b7eb0818
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 23:11:21 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -147,7 +147,8 @@ const Navbar = () => {
                     <button
                       type="button"
                       className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                     >
+                     onClick={() => setOpen(!open)}
+                    >
                       <span className="sr-only">Close menu</span>
                       {/* Heroicon name: outline/x */}
                       <svg
