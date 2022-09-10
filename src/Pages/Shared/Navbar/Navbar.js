ab12a99786c78a5ab7182c4f91835f5467f26f55
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 23:23:01 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -39,6 +39,7 @@ const Navbar = () => {
                 </Link>
               </motion.div>
               <div className="-mr-2 -my-2 md:hidden">
+<ThemeToggle />
                 <button
                   type="button"
                   className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
