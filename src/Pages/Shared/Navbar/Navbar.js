23eac18a4197bec94e94fd0c8a416bf70cc90c56
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 13:34:18 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
               >
                 <Link to="/">
                   <img
-                    className="h-12 md:w-40 sm:w-32"
+                    className="h-10 md:w-40 sm:w-28 sm:h-8"
                     src={logo}
                     alt="logo"
                   />
@@ -106,7 +106,7 @@ const Navbar = () => {
                   rel="noreferrer"
                   href="https://drive.google.com/file/d/1ZBioCcrhS1JKp-6Azu61jH2RSeBtu_bB/view?usp=sharing"
                   target="_blank"
-                  className="ml-8 mr-12 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                  className="ml-8 mr-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Resume
                 </a>
