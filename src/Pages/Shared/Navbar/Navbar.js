a22ca1c855a3fc7cee1dc688dfab632b39d8ac5f
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 21:47:58 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -145,7 +145,7 @@ const Navbar = () => {
                   </div>
                   <div className="-mr-2">
                   <div className="flex items-center justify-between">
-                    <div className="mr-4"><ThemeToggle /></div>
+                    <ThemeToggle />
                     <button
                       type="button"
                       className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
