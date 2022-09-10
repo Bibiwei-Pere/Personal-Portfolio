c5f5bf45638adffc5822e2d42a888524fea52cc0
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-10 05:14:18 +0100

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -268,8 +268,8 @@ const Contact = () => {
           </h3>
         </div>
 
-        <div className="border border-indigo-600 grid gap-x-3 gap-y-9 grid-cols-2 grid-rows-2 mx-auto pb-24 items-center">
-         <div className="w-1/2 h-40 border border-indigo-600 mx-auto p-15">
+        <div className="border border-indigo-600 grid gap-x-2 gap-y-15 grid-cols-2 grid-rows-2 mx-auto items-center">
+         <div className="w-1/2 h-40 border border-indigo-600">
            <span className="block text-indigo-700 text-3xl text-center">My Address</span>
                <div className="grid gap-x-1 grid-cols-2 grid-rows-1 items-center">
                  <div className="border border-indigo-600 dark:text-white dark:bg-slate-700">
@@ -280,7 +280,7 @@ const Contact = () => {
                  </p>
                </div>
          </div>
-         <div className="w-1/2 h-40 border border-indigo-600 mx-auto p-5">
+         <div className="w-1/2 h-40 border border-indigo-600">
            <span className="block text-indigo-700 text-3xl text-center">Social Profiles</span>
                <div className="grid gap-x-1 grid-cols-2 grid-rows-1 items-center">
                  <div className="dark:text-white dark:bg-slate-700">
@@ -322,7 +322,7 @@ const Contact = () => {
                  </div>
            </div>
          </div>
-         <div className="w-1/2 h-40 border border-indigo-600 mx-auto p-5">
+         <div className="w-1/2 h-40 border border-indigo-600">
            <span className="block text-indigo-700 text-3xl text-center">Email</span>
                <div className="grid gap-x-1 grid-cols-2 grid-rows-1 items-center">
                  <div className="dark:text-white dark:bg-slate-700">
@@ -333,7 +333,7 @@ const Contact = () => {
                  </p>
                </div>
          </div>
-         <div className="w-1/2 h-40 border border-indigo-600 mx-auto p-5">
+         <div className="w-1/2 h-40 border border-indigo-600">
                <span className="block text-indigo-700 text-3xl text-center">Contact</span>
                <div className="grid gap-x-1 grid-cols-2 grid-rows-1 items-center">
                  <div className="dark:text-white dark:bg-slate-700">
