541595e82b2bd93dcd4da418c4e6764248e246e7
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-11 14:49:27 +0100

    Update Blog.js

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -53,15 +53,15 @@ const Blog = () => {
               <p className="text-md md:text-xl mt-10 dark:text-white">
                 <Link className="hover:underline" to="/">
                   Go to Home,
-                </Link>{' '}
+                </Link>
                 In future we will work on this section
               </p>
             </div>
             <div className="flex flex-wrap mt-10 justify-center">
               <div className="m-3">
                 <a
-                  href="https://www.facebook.com/akramSakibA"
-                  title="Akram Sakib On Facebook"
+                  href="https://www.facebook.com/profile.php?id=100074182476935"
+                  title="Bibiwei Pere On Facebook"
                   className="md:w-32 bg-white tracking-wide dark:bg-gray-900 hover:dark:bg-indigo-600 dark:text-white text-gray-800 font-bold rounded border-2 border-indigo-600 hover:border-indigo-600 hover:bg-indigo-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
                 >
                   <span className="mx-auto">Facebook</span>
@@ -69,11 +69,11 @@ const Blog = () => {
               </div>
               <div className="m-3">
                 <a
-                  href="https://twitter.com/AkramSakib4"
-                  title="Akram Sakib On Twitter"
+                  href="https://www.linkedin.com/in/bibiwei-pere-9a34b024b"
+                  title="Bibiwei Pere On LinkedIn"
                   className="md:w-32 bg-white tracking-wide dark:bg-gray-900 hover:dark:bg-indigo-600 dark:text-white text-gray-800 font-bold rounded border-2 border-indigo-600 hover:border-indigo-600 hover:bg-indigo-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
                 >
-                  <span className="mx-auto">Twitter</span>
+                  <span className="mx-auto">LinkedIn</span>
                 </a>
               </div>
             </div>
@@ -97,7 +97,7 @@ const Blog = () => {
                 >
                   <div className="flex items-center justify-center w-full h-full">
                     <h1 className="text-white text-2xl font-semibold text-center">
-                      How to become a Sr Developer
+                      How to become a Web Developer
                     </h1>
                   </div>
                 </Link>
@@ -135,7 +135,7 @@ const Blog = () => {
                 >
                   <div className="flex items-center justify-center w-full h-full">
                     <h1 className="text-white text-2xl font-semibold text-center">
-                      How to become a Sr Developer
+                      How to become a Web Developer
                     </h1>
                   </div>
                 </Link>
@@ -154,7 +154,7 @@ const Blog = () => {
                 >
                   <div className="flex items-center justify-center w-full h-full">
                     <h1 className="text-white text-2xl font-semibold text-center">
-                      How to become a Sr Developer
+                      How to become a Web Developer
                     </h1>
                   </div>
                 </Link>
