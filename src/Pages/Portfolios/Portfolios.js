77e8b66aa52c1dad8665ba7221b0582c87604c15
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-11 17:31:43 +0100

    Update Portfolios.js

--- src/Pages/Portfolios/Portfolios.js
+++ src/Pages/Portfolios/Portfolios.js
@@ -3,7 +3,7 @@ import React from 'react';import Projects from "./../Home/Projects/Projects";
 const Portfolios = () => {
     return (
       <>
-       <div className="py-20 sm:pt-10">
+       <div className="py-20 sm:pt-5">
         <Projects />
       </div>
       </>
