fadbc038b8f5184577e70f2ac94e2590ea1109d4
Bibiwei-Pere <bibiweipere@gmail.com>
2022-09-12 21:55:06 +0100

    2 files modified, 1 added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import './About.css';
 import { Link } from 'react-router-dom';
-import hero from "./../../Assets/Images/hero.jpg";
+import hero from './../../Assets/Images/hero.jpg';
 
 const About = () => {
   return (
@@ -14,15 +14,18 @@ const About = () => {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h2 className="text-4xl md:text-5xl text-left tracking-tight font-extrabold  text-dark dark:text-white sm:leading-none">
-            Web Application
-            <span className="text-indigo-600 dark:text-indigo-500"> Developer</span>
-          </h2>
-          <p className="mt-3 pb-16 text-left text-3xl dark:text-white text-dark sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
-            A passionate individual who always thrive to work on end to end
-            products which develop sustainable and scalable social and technical
-            systems to create impact. I’m currently learning JavaScript more
-            advance
-          </p>
+              Web Application
+              <span className="text-indigo-600 dark:text-indigo-500">
+                {' '}
+                Developer
+              </span>
+            </h2>
+            <p className="mt-3 pb-16 text-left text-3xl dark:text-white text-dark sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
+              A passionate individual who always thrive to work on end to end
+              products which develop sustainable and scalable social and
+              technical systems to create impact. I’m currently learning
+              JavaScript more advance
+            </p>
             <div className="flex justify-center">
               <a
                 rel="noreferrer"
@@ -40,7 +43,7 @@ const About = () => {
             </div>
           </div>
           <div className="border-3 border-dark">
-          <img
+            <img
               className="object-cover object-center rounded-lg"
               alt="hero"
               src={hero}
@@ -50,16 +53,18 @@ const About = () => {
       </section>
       <section className="edu_cation pb-5">
         <div className="educationWrapper">
-          <div className="dark:text-white text-4xl text-center uppercase font-extrabold">Education</div>
+          <div className="dark:text-white text-4xl text-center uppercase font-extrabold">
+            Education
+          </div>
 
           <div className="timeline">
             <ul>
               <li>
                 <div className="content">
-                  <h3 className="dark:text-white">Primary School Certificate</h3>
-                  <p className="dark:text-gray-400">
-                    Burutu Primary School
-                  </p>
+                  <h3 className="dark:text-white">
+                    Primary School Certificate
+                  </h3>
+                  <p className="dark:text-gray-400">Burutu Primary School</p>
                   <p className="dark:text-gray-400">
                     Completed PSC From Burutu Primary School
                   </p>
@@ -79,9 +84,7 @@ const About = () => {
                   <h3 className="dark:text-white">
                     Secondary School Certificate
                   </h3>
-                  <p className="dark:text-gray-400">
-                    Burutu Grammar School
-                  </p>
+                  <p className="dark:text-gray-400">Burutu Grammar School</p>
                   <p className="dark:text-gray-400">
                     Completed SSC From Burutu Grammar School
                   </p>
@@ -99,9 +102,7 @@ const About = () => {
               <li>
                 <div className="content">
                   <h3 className="dark:text-white">Medicine and Surgery</h3>
-                  <p className="dark:text-gray-400">
-                    Niger Delta University
-                  </p>
+                  <p className="dark:text-gray-400">Niger Delta University</p>
                   <p className="dark:text-gray-400">
                     I'm currently studying in Niger Delta University
                   </p>
@@ -126,16 +127,18 @@ const About = () => {
         <div className="container mx-auto flex px-10 md:flex-row flex-col items-center">
           <div className="skills_left">
             <div className="my-8 text-center">
-              <span className="text-indigo-700 text-2xl md:text-3xl md:font-bold">Skills</span>
+              <span className="text-indigo-700 text-2xl md:text-3xl md:font-bold">
+                Skills
+              </span>
               <h3 className="text-3xl md:text-4xl uppercase font-extrabold">
                 Programming Skills
               </h3>
             </div>
             <div className="my-8">
               <p>
-                For about a year now, have been accomplishing enough skills
-                with modern Web Development, new generation web and app
-                programming language.
+                For about a year now, have been accomplishing enough skills with
+                modern Web Development, new generation web and app programming
+                language.
               </p>
             </div>
             <div className="progress_bar w-full md:w-3/6 mx-auto">
@@ -176,7 +179,9 @@ const About = () => {
                 </div>
               </div>
               <div className="my-8">
-                <div className="mb-1 text-base font-medium">Styled Components</div>
+                <div className="mb-1 text-base font-medium">
+                  Styled Components
+                </div>
                 <div className="w-full h-4 bg-transparent rounded-full">
                   <div
                     className="h-4 bg-indigo-600 rounded-full"
---
+++ src/Pages/Licensing/Licensing.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Licensing = () => {
+  return (
+    <section className="container px-6 mx-auto relative pt-20 pb-20">
+      <div className="pt-8 md:pt-16 lg:pt-20 px-6">
+        <h2 className="mb-2 text-4xl md:text-5xl text-left tracking-tight font-extrabold  text-dark dark:text-white sm:leading-none">
+          MIT
+          <span className="text-indigo-600 dark:text-indigo-500">Licence</span>
+        </h2>
+        <p className="text-left text-3xl dark:text-white text-dark sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
+          Permission is hereby granted, free of charge, to any person obtaining
+          a copy of this software and associated documentation files (the
+          "Software"), to deal in the Software without restriction, including
+          without limitation the rights to use, copy, modify, merge, publish,
+          distribute, sublicense, and/or sell copies of the Software, and to
+          permit persons to whom the Software is furnished to do so, subject to
+          the following conditions: The above copyright notice and this
+          permission notice shall be included in all copies or substantial
+          portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT
+          WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+          THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+          AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+          HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+          IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+          IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+          SOFTWARE.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Licensing;
