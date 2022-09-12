5051d0a66829ea514c02fe81e8180c1dcdc212d2
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-12 06:55:20 +0100

    Update Navbar.js

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -129,7 +129,7 @@ const Navbar = () => {
             className={
               open
                 ? "opacity-100 scale-100 transition ease-out duration-200 absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-                : "opacity-0 scale-95 absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                : "invisible opacity-0 scale-95 absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
             }
           >
             <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
