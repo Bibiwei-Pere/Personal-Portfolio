945f36b464069e498c711b2a472b17a0836861fb
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-13 11:59:11 +0100

    Update Licensing.js

--- src/Pages/Licensing/Licensing.js
+++ src/Pages/Licensing/Licensing.js
@@ -36,7 +36,7 @@ const Licensing = () => {
         <div className="md:flex items-center justify-evenly mt-8">
           <div className="p-2">
             <h3 className="text-left dark:text-white text-3xl font-bold">Permissions</h3>
-            <ul className="h-10 text-left text-3xl dark:text-white text-dark mt-5 text-lg">
+            <ul className="h-15 text-left text-3xl dark:text-white text-dark mt-5 text-lg">
               <li class="flex items-center space-x-3">
                 <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                 <span>Commercial Use</span>
