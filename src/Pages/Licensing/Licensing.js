219107e4ebc47a381aaeac61cedba6e0d42ca6bb
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-13 13:12:20 +0100

    Update Licensing.js

--- src/Pages/Licensing/Licensing.js
+++ src/Pages/Licensing/Licensing.js
@@ -33,10 +33,10 @@ const Licensing = () => {
           IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
           SOFTWARE.
         </p>
-        <div className="md:flex items-center justify-evenly mt-8">
-          <div className="p-2">
+        <div className="md:flex grid items-center justify-evenly mt-8">
+          <div className="p-2 h-15">
             <h3 className="text-left dark:text-white text-3xl font-bold">Permissions</h3>
-            <ul className="h-15 text-left text-3xl dark:text-white text-dark mt-5 text-lg">
+            <ul className="text-left text-3xl dark:text-white text-dark mt-5 text-lg">
               <li class="flex items-center space-x-3">
                 <svg class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                 <span>Commercial Use</span>
@@ -55,18 +55,18 @@ const Licensing = () => {
               </li>
               </ul>
           </div>
-          <div className="p-2 mt-5 md:mt-0">
+          <div className="h-15 p-2 mt-5 md:mt-0">
             <h3 className="text-left dark:text-white text-3xl font-bold">Conditions</h3>
-            <ul className="h-10 text-left text-3xl dark:text-white text-dark mt-5 text-lg">
+            <ul className="text-left text-3xl dark:text-white text-dark mt-5 text-lg">
               <li class="flex items-center space-x-3">
                 <svg class="flex-shrink-0 w-6 h-4 text-indigo-600 dark:text-blue-400" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="-1.76731 -1.89367 3.401 3.68">  <path d="M -1.426 0.002 C -1.393 -2.083 1.326 -2.142 1.302 -0.001 V 0 H 1.632 H 1.457 H -0.219 V -0.136 H -0.351 C -0.612 -0.14 -0.608 -0.494 -0.336 -0.508 H 0.144 V -1 H 0.143 C 0.169 -0.632 -0.24 -0.677 -0.229 -0.999 C -0.242 -1.288 0.138 -1.358 0.144 -1 H 0.144 V 0.002 V 0.597 H 0.22 C 0.507 0.59 0.55 1.011 0.223 1.029 H -0.321 C -0.595 1.034 -0.58 0.602 -0.304 0.597 H -0.219 V 0 V 0 V 0 V 0 V 0 H 1.302 C 1.209 2.211 -1.544 1.715 -1.425 0.002 H 1.632 C 1.731 -2.462 -1.73 -2.588 -1.767 0.001 C -1.804 2.143 1.461 2.607 1.632 0.002"/></svg>
                 <span>License and copyright notice</span>
               </li>
             </ul>
           </div>
-          <div className="p-2 mt-5 md:mt-0">
+          <div className="h-15p-2 mt-5 md:mt-0">
             <h3 className="text-left dark:text-white text-3xl font-bold">Limitations</h3>
-            <ul className="h-15 text-left text-3xl dark:text-white text-dark mt-5 text-lg">
+            <ul className="text-left text-3xl dark:text-white text-dark mt-5 text-lg">
               <li class="flex items-center space-x-3">
                <svg class="flex-shrink-0 w-4 h-3 text-red-500 dark:text-red-400" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="-0.857 -1.011 2.179 2.072">  <path d="M -0.857 -0.731 L 0 0 L -0.798 0.815 L -0.516 1.061 L 0.237 0.24 L 1.069 1.043 L 1.297 0.77 L 0.488 0.018 L 1.322 -0.735 L 1.058 -1 L 0.242 -0.223 L -0.572 -1.011"/></svg>
                <span>Trademark use</span>              
