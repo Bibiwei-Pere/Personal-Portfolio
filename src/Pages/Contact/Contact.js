9b70e1090e82a5a6615e3fe8503efa728b1e4e79
Bibiwei Pere <bibiweipere@gmail.com>
2022-09-13 13:21:55 +0100

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -129,7 +129,7 @@ const Contact = () => {
       <Helmet>
         <title>Bibiwei Pere - Contact</title>
       </Helmet>
-      <section className="contact_section text-gray-700 body-font dark:text-white py-24 md:py-20" ref={ref}>
+      <section className="contact_section text-gray-700 body-font dark:text-white container px-6 mx-auto relative pt-20 pb-20" ref={ref}>
         <ToastContainer
           position="top-right"
           autoClose={3000}
